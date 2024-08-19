ef5adc91ecdc0c7d9de1150c813f1c2dc9cfc744
Abel Mulugeta <abel.mulugeta.dagne@gmail.com>
2024-08-19 10:37:04 +0300

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 print(
-    "{}".format("".join(chr(i) for i in range(97, 123) if i not in (101, 113))), end=""
-)
+    "{}".format(
+        "".join(chr(i) for i in range(97, 123) if i not in (101, 113))
+        ), end="")
