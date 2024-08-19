c622fe73a3bdac75b169e6929255fbdf9d448eaf
Abel Mulugeta <abel.mulugeta.dagne@gmail.com>
2024-08-19 11:01:08 +0300

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
         if 97 <= ord(char) <= 122:
-            char = chr(ord(char) - 32)
-        print(char, end="")
-    print()
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
