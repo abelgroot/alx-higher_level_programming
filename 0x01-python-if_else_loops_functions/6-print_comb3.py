7811988893412bb5c15902dc89b7262584d2cc39
Abel Mulugeta <abel.mulugeta.dagne@gmail.com>
2024-08-19 10:55:21 +0300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+first = True
 for i in range(10):
     for j in range(i + 1, 10):
-        if i != j:
-            if i != 0 or j != 1:
-                print(", ", end="")
-                print("{:02d}".format(i * 10 + j), end="")
+        if not first:
+            print(", ", end="")
+        print("{:02d}".format(i * 10 + j), end="")
+        first = False
 print()
