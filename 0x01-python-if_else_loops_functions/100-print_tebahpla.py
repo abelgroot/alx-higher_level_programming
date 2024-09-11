b43a76dfc3bf340c6ad0661b1cedc37bdba52674
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-09-11 21:28:28 +0300

    100-printalpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 def print_tebahpla():
     for i in range(25, -1, -1):
         if i % 2 == 0:
-            print(chr(ord('a') + i), end="")
+            print(chr(ord("a") + i), end="")
         else:
-            print(chr(ord('A') + i), end="")
+            print(chr(ord("A") + i), end="")
