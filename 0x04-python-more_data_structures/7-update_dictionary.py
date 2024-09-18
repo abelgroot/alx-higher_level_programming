449a65902feef16d5e99c28f5670e410c8ca7621
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-09-18 23:25:36 +0300

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    # Use lambda and map to handle key update or addition
+    # Use filter to find if the key exists
+    existing_keys = list(filter(lambda k: k == key, a_dictionary.keys()))
+
+    # Update or add the key-value pair
     updated_dict = dict(
         map(
             lambda item: (item[0], value) if item[0] == key else item,
             a_dictionary.items(),
         )
     )
-    if key not in updated_dict:
+
+    # If the key doesn't exist, add it
+    if not existing_keys:
         updated_dict[key] = value
+
     return updated_dict
