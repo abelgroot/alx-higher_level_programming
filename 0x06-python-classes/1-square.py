e1f0e47c1ef03822f351bc292231338b573d8317
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-01 23:31:22 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """A class that defines a square by size."""
+
+    def __init__(self, size):
+        """Instantiate a square with a given size."""
+        self.__size = size  # Private attribute
