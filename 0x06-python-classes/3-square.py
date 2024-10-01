5b09672950012d9d5399ab9b84d778a39add6a12
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-01 23:36:28 +0300

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """A class that defines a square by size."""
+
+    def __init__(self, size=0):
+        """Instantiate a square with a given size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size  # Private attribute
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size**2  # Area of the square
