d9816cc589bc6e9f8eab4bd704b6d14f6b161779
Abel Mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-06 16:12:07 +0300

    19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def copy_list(l):
+def copy_list(input_list):
     """Return a shallow copy of a list.
 
     Args:
-        l (list): The list to copy.
+        input_list (list): The list to copy.
 
     Returns:
         list: A new list that is a copy of the input list.
     """
-    return l[:]
+    return input_list[:]
