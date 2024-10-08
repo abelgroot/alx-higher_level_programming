24b0084aba1be6fff77d51418f2c66080fbec837
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-08 22:18:49 +0300

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+"""
+
 
 class Rectangle:
+    """
+    Represents a rectangle.
+    """
+
     def __init__(self, width=0, height=0):
+        """
+        Initializes a rectangle with optional width and height.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Getter for the width attribute."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter for the width attribute with type and value checks."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,10 +32,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Getter for the height attribute."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter for the height attribute with type and value checks."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -30,14 +45,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Returns the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Returns the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        """Returns a string representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(["#" * self.__width for _ in range(self.__height)])
+        return "\n".join("#" * self.__width for _ in range(self.__height))
