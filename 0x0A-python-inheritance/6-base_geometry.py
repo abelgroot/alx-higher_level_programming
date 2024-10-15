521f3f9990273e03e9bfd940d1ae3c22d788e1d8
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-15 22:30:00 +0300

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+class BaseGeometry:
+    """A class that represents basic geometry."""
+
+    def area(self):
+        """Calculate the area.
+
+        Raises:
+            Exception: area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
