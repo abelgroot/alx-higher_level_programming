b2d5627daaf66fa2bb3e781690133877b2c6fc6c
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-16 22:06:48 +0300

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a rebellious integer class MyInt."""
+
+
+class MyInt(int):
+    """A class representing a rebel integer that inverts == and != operators."""
+
+    def __eq__(self, other):
+        """Override == operator to behave as !=."""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Override != operator to behave as ==."""
+        return super().__eq__(other)
