351ae33ac30e6ccee284393e790dfc5b7ee3521f
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-16 23:01:57 +0300

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,16 @@
-ass_to_json(obj):
-        """
-            Returns the dictionary description with simple data structure
-                for JSON serialization of an object.
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
 
-                    Args:
-                            obj (object): An instance of a class.
+    Args:
+        obj (object): An instance of a class.
 
-                                Returns:
-                                        dict: A dictionary containing serializable attributes of the object.
-                                            """
-                                                return {
-                                                                key: value for key, value in obj.__dict__.items()
-                                                                        if isinstance(value, (list, dict, str, int, bool))
-                                                                            }
+    Returns:
+        dict: A dictionary containing serializable attributes of the object.
+    """
+    return {
+        key: value
+        for key, value in obj.__dict__.items()
+        if isinstance(value, (list, dict, str, int, bool))
+    }
