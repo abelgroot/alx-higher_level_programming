4a64d16830012bcc8ec6fe68c344f3c3f5047bf9
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-22 21:21:25 +0300

    base.py to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base class module"""
+import json
 
 
 class Base:
@@ -14,3 +15,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+        If list_dictionaries is None or empty, returns the string "[]".
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
