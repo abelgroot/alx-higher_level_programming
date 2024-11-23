ee2cd67bc1f2b59877d845538a91d9bfd0d09388
Abel Mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-23 17:40:39 +0300

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Script to print the full description of the table first_table
-SELECT TABLE_NAME, CREATE_TABLE 
-FROM information_schema.tables
-WHERE table_schema = DATABASE() 
-AND table_name = 'first_table';
+SHOW CREATE TABLE first_table;
