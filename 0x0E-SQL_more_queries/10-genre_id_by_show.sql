e846ce804659029e4ed624620000a092356bc0e3
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-24 23:36:11 +0300

    10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Query to list all shows that have at least one genre linked
+-- Each record will show tv_shows.title and tv_show_genres.genre_id
+-- The results are sorted by tv_shows.title and tv_show_genres.genre_id in ascending order
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
