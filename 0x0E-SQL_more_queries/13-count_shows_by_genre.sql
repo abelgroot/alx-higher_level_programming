7fed30a83ce3270a6c83f0c88c58a14e7f90dbe2
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-24 23:41:04 +0300

    13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Query to list all genres and the number of shows linked to each genre
+-- The result is sorted in descending order by the number of shows linked
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
