b9e7c15758a39003e4a80ea23cc9d17b2891da39
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-24 20:45:41 +0300

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,11 @@
 -- Script to convert hbtn_0c_0 database, first_table table, and name field to utf8mb4
 USE hbtn_0c_0;
 
--- 1. Change the character set and collation of the database hbtn_0c_0
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- Convert the database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- 2. Change the character set and collation of the table first_table
-ALTER TABLE first_table
+-- Convert the table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the `name` field in `first_table` to UTF8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
