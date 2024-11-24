b68e5084c760efa1ee826f67a639c957283e1397
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-24 23:37:37 +0300

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@
 -- The results are sorted by tv_shows.title and tv_show_genres.genre_id in ascending order
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
