67d97737adfd5200b5e7478a79f227e247b74b8b
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-24 23:40:01 +0300

    12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Query to list all shows that do not have a genre linked
+-- The result is sorted by tv_shows.title and tv_show_genres.genre_id in ascending order
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC;
