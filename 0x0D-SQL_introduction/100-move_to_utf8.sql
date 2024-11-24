bf6c95a508c6abbd4b85abb759b8fb01b2f84784
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-24 20:51:20 +0300

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Script to convert hbtn_0c_0 database, first_table table, and name field to utf8mb4
-USE hbtn_0c_0;
+USE  hbtn_0c_0;
 
 -- Convert the database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
