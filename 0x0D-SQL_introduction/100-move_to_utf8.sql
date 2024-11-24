238f2c11afca5e6585b70e41e27a0b060da37759
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-24 20:54:40 +0300

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -20,7 +20,7 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Convert the table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Modify the `name` column to ensure only the collation is applied
+-- Explicitly set only the collation for the `name` field
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
 
 -- Display the description of the table
