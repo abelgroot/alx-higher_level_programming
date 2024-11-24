e476947f180a97e64003a8c0d8ed5f429cc2fbdd
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-11-24 23:10:20 +0300

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Check if user_0d_1 already exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
