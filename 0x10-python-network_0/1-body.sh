64603d933df400a1485a30376e4e94aa864ebfe1
abel mulugeta <abel.mulugeta.dagne@gmail.com>
2025-01-25 16:31:31 +0300

    1-body.sh updated

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body of the response if the status is 200
-curl -s -X GET "$1" -o /dev/stdout -w "%{http_code}" | sed '$ d'
+curl -sL GET "$1"
