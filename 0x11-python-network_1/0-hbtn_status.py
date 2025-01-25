e083f50eb33b1e6a07d3df1f31e097b987ce071c
groot <abel.mulugeta.dagne@gmail.com>
2025-01-25 20:29:36 +0300

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script fetches the status from
+https://alx-intranet.hbtn.io/status using the urllib package.
+It displays the body of the response in a specific format.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
